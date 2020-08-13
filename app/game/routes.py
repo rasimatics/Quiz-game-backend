38aa0ac8f3505f91009530207e2ca8b9bd62b8fc
Rasim <rasimmammadoff1@gmail.com>
2020-08-13 09:48:24 +0400

    Structure with comments

--- app/game/routes.py
+++ app/game/routes.py
@@ -7,38 +7,29 @@ from app.models import GameRoom, Player
 
 # join to existing room else create room
 class CreateOrJoin(Resource):
-
     def post(self):
-        gameroom = GameRoom.objects(waiting=True).order_by("created_at").first()
+        gameroom = GameRoom.objects(
+            waiting=True).order_by("created_at").first()
         player = Player(name="Rasim")
         # join existing room
         if gameroom:
-           gameroom.waiting = False
-           gameroom.members.append(player)
-           gameroom.save()
-           # start game
+            gameroom.waiting = False
+            gameroom.members.append(player)
+            gameroom.save()
+            # socket join_room()
+            # start game
 
         # create a new room
         else:
-           gameroom = GameRoom()
-           gameroom.members.append(player)
-           gameroom.save()
-           # wait
-           # if not user connect bot
-           # start game
+            gameroom = GameRoom()
+            gameroom.members.append(player)
+            gameroom.save()
+            # socket join_room()
+            # wait
+            # if not user connect bot
+            # start game
 
         return jsonify(gameroom)
 
-
-"""
-   if waiting room exists:
-      join to this room
-      game info (start game)
-   else:
-      create a new room
-      after 10s get request from front (no waiting room)
-      join bot player
-      game info (start game)(return game data(questions and word))
-"""
-
 api.add_resource(CreateOrJoin, '/start-game/')
+
--- app/game/sockets.py
+++ app/game/sockets.py
@@ -1,3 +1,8 @@
+# disconnect
+# def handle_disconnect()
+
+# join to the room
+# def join_room()
 
 # give information about game-status
 # def game_info() different for each user related to username
